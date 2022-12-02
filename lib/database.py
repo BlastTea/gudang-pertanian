6cdc426c4af339718861912989dfdd9b9c7ecac8
BlastTea <arya.jbr.999@gmail.com>
2022-12-02 22:50:36 +0700

    fix transaction merge

--- lib/database.py
+++ lib/database.py
@@ -94,6 +94,9 @@ def readTransactions(whereIdRack=-1, merge=True) -> pd.DataFrame:
 
     if whereIdRack != -1:
         transactions.query(f'IdRak == {whereIdRack}', inplace=True)
+        
+    if not merge:
+        return transactions
     transactions = transactions.merge(items, on='IdBarang')
     transactions = transactions.merge(racks, on='IdRak')
     longRottens = transactions['LamaBusuk'].values
